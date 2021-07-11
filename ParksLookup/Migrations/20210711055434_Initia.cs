62c8ec42c813a96689fa2247d3ea57d55eacd018
Saoud <saoud@saoud.dev>
2021-07-10 22:54:51 -0700

    adding working migrations

---
+++ ParksLookup/Migrations/20210711055434_Initia.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class Initia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ParkName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    ParkLocation = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "ParkLocation", "ParkName" },
+                values: new object[,]
+                {
+                    { 1, "Washington", "Mount Rainier" },
+                    { 2, "Washington", "North Cascades" },
+                    { 3, "Utah", "Zion" },
+                    { 4, "Virginia", "Shenandoah" },
+                    { 5, "California", "Redwood" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
